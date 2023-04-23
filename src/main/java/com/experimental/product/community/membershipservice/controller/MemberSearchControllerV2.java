46032ecec8531bd5a51a24a5693691206df997ea
Anik Roy <01anikroy04@gmail.com>
2023-04-23 13:56:06 +0530

    MemberSearchControllerV2 MemberRepositoryV2 MemberSearchServiceV2 in java

--- src/main/java/com/experimental/product/community/membershipservice/controller/MemberSearchControllerV2.java
+++ src/main/java/com/experimental/product/community/membershipservice/controller/MemberSearchControllerV2.java
@@ -3,6 +3,7 @@ package com.experimental.product.community.membershipservice.controller;
 
 import com.experimental.product.community.membershipservice.client.response.MemberResponse;
 import com.experimental.product.community.membershipservice.service.MemberSearchService;
+import com.experimental.product.community.membershipservice.service.MemberSearchServiceV2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +17,7 @@ import java.util.List;
 @RequestMapping("v2/members")
 public class MemberSearchControllerV2 {
     @Autowired
-    private MemberSearchService memberSearchService;
+    private MemberSearchServiceV2 memberSearchService;
 
     @GetMapping()
     public ResponseEntity<List<MemberResponse>> getAll()

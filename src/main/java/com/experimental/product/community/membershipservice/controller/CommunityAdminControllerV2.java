ad83150f1a16b5d00e9e7730778a0eed4de6197a
royanik17 <63785725+royanik17@users.noreply.github.com>
2023-06-10 10:09:38 +0530

    Import Error Fixing (#9)
    * Import Error Fixing
    
    * Folder Structure fix

--- src/main/java/com/experimental/product/community/membershipservice/controller/CommunityAdminControllerV2.java
+++ src/main/java/com/experimental/product/community/membershipservice/controller/CommunityAdminControllerV2.java
@@ -1,13 +1,13 @@
 package com.experimental.product.community.membershipservice.controller;
 
 import com.experimental.product.community.membershipservice.client.request.CreateCommunityRequest;
-import com.experimental.product.community.membershipservice.client.request.UpdateCommunityRequest;
-import com.experimental.product.community.membershipservice.client.request.UpdateMemberRequest;
 import com.experimental.product.community.membershipservice.service.CommunityAdminServiceV2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("v2/communities")
--- src/main/java/com/experimental/product/community/membershipservice/service/CommunityAdminServiceV2.java
+++ src/main/java/com/experimental/product/community/membershipservice/service/CommunityAdminServiceV2.java
@@ -1,18 +1,10 @@
 package com.experimental.product.community.membershipservice.service;
 
 import com.experimental.product.community.membershipservice.client.request.CreateCommunityRequest;
-import com.experimental.product.community.membershipservice.client.request.UpdateCommunityRequest;
-import com.experimental.product.community.membershipservice.client.request.UpdateMemberRequest;
 import com.experimental.product.community.membershipservice.entity.Community;
-import com.experimental.product.community.membershipservice.entity.Member;
-import com.experimental.product.community.membershipservice.repository.CommunityRepository;
 import com.experimental.product.community.membershipservice.repository.CommunityRepositoryV2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.Objects;
-import java.util.Optional;
 
 @Service
 public class CommunityAdminServiceV2 {

dbff43cef8f5c246a956f03be60209feb4f4f75f
LAPTOP\anik0 <01anikroy04@gmail.com>
2023-07-03 09:33:07 +0530

    Features[Add JOSN Property]

--- src/main/java/com/experimental/product/community/membershipservice/client/request/CreateCommunityRequestV2.java
+++ src/main/java/com/experimental/product/community/membershipservice/client/request/CreateCommunityRequestV2.java
@@ -3,6 +3,7 @@ package com.experimental.product.community.membershipservice.client.request;
 import com.experimental.product.community.membershipservice.client.ContactType;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 import java.util.Map;
@@ -10,13 +11,28 @@ import java.util.Map;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateCommunityRequestV2 {
+    @JsonProperty("name")
     private String name;
+
+    @JsonProperty("description")
     private String description;
+
+    @JsonProperty("address")
     private String address;
+
+    @JsonProperty("contacts")
     private Map<ContactType, String> contacts;
+
+    @JsonProperty("emergencyContacts")
     private List<String> emergencyContacts;
+
+    @JsonProperty("isGated")
     private Boolean isGated;
+
+    @JsonProperty("hasPool")
     private Boolean hasPool;
+
+    @JsonProperty("hasGym")
     private Boolean hasGym;
 
     public String getName() {
